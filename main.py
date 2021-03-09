ce0c8afb71a53bfc461d3b51de9363ffce40b410
Exctues <alexnamecode@gmail.com>
2021-03-09 07:29:54 +0300

    баг-фиксы, комменты+логи

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ import numpy as np
 from fastapi import FastAPI, Request, UploadFile, File, Body
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import StreamingResponse
-from pydantic import BaseModel
 from typing import List
-from io import BytesIO
 
 # глобальные переменные, будут заполнены при парсинге в мейне
 SAVE_FOLDER = ""
@@ -37,19 +34,47 @@ def upload(source, destination) -> None:
     my_disk.upload(source, destination)
 
 
-def process(img):
-    """Здесь нейронка вызывается и получаем картинку"""
-    return img
+def process(old_img, new_img):
+    """
+    Здесь нейронка вызывается и получаем картинку
+    """
+    merged_img = old_img
+    return merged_img
 
 
-def calculate_center(points: List[List[int]]):
-    """Находим центр масс всех точек, смотрим концентрацию взгляда"""
-    pass
+def calculate_center(points: List[List[int]]) -> List[int]:
+    """
+    Находим центр масс(среднее) всех точек, смотрим концентрацию взгляда.
+    """
+    # Подумать нужен ли нам центроид (по теореме Грина) или все-таки среднее?
+    x = [p[0] for p in points]
+    y = [p[1] for p in points]
+    center_average = [int(sum(x) / len(points)), int(sum(y) / len(points))]
+    return center_average
 
 
-def classify(center: int) -> str:
-    """Классицифруем, возможные исходы: top_left, top_right, bottom_left, bottom_right"""
-    pass
+def classify(center: List[int]) -> str:
+    """
+    Классифицифруем, возможные исходы: top_left, top_right, bottom_left, bottom_right
+    допустим что точки принимаю значения x - [0, 1920] y - [0, 1080]
+    и начало отсчета в левом верхнем углу, луч x направлен вправо, луч y вниз
+    """
+    x_half = 1920 // 2
+    y_half = 1080 // 2
+    class_type = "bottom_right"
+    x = center[0]
+    y = center[1]
+    if x < x_half and y < y_half:
+        class_type = "top_left"
+    elif x < x_half and y > y_half:
+        class_type = "bottom_left"
+    elif x > x_half and y < y_half:
+        class_type = "top_right"
+    else:
+        class_type = "bottom_right"
+
+    print(f"calculated center: x: {x}, y: {y} => type is {class_type}")
+    return class_type
 
 
 # routes
@@ -60,38 +85,94 @@ async def root(request: Request):
     })
 
 
-@app.post("/points")
-async def points_process(points: List[List[int]] = Body(...)):
-    return {"received points": points}
-
-
 @app.post("/face")
 async def face_process(uuid: str, image: UploadFile = File(...), points: str = Body(...)):
-    """points: {"points": [[1,2,3],[4,5,6]]}"""
-    # image = face_data.image
-    # uuid = face_data.uuid
-    received_points = points
-    points = json.loads(received_points)['points']
-    print(f"Received uuid: {uuid} --- points: {[points]}")
+    """
+    Обрабатываем входящую картинку и хит-мапу в виде точек
+    формат points: {"points": [[1,2,3],[4,5,6]]}
+    """
+    print(f"--------------- New request -------------")
+    input_points = points
+    points = json.loads(input_points)['points']
+    print(f"Received uuid: , size of points {len(points)}")
 
-    remote_img_path = YADISK_FOLDER + os.sep + uuid + ".png"
-    local_img_path = SAVE_FOLDER + os.sep + uuid + ".png"
+    # классицифируем
+    class_type = classify(calculate_center(points))
+    print(f"Classified to type {class_type}")
 
-    remote_json_path = YADISK_FOLDER + os.sep + uuid + ".json"
-    local_json_path = SAVE_FOLDER + os.sep + uuid + ".json"
+    # прописываем локальные и удаленные (которые на yadisk) пути
+    remote_img_path = YADISK_FOLDER + os.sep + class_type + os.sep + uuid + ".png"
+    local_img_path = SAVE_FOLDER + os.sep + class_type + os.sep + uuid + ".png"
 
-    # читаем картинку
+    remote_json_path = YADISK_FOLDER + os.sep + class_type + os.sep + uuid + ".json"
+    local_json_path = SAVE_FOLDER + os.sep + class_type + os.sep + uuid + ".json"
+
+    remote_last_same_img_path = YADISK_FOLDER + os.sep + "last_" + class_type + ".png"
+    local_last_same_img_path = SAVE_FOLDER + os.sep + "last_" + class_type + ".png"
+
+    # достаем последнюю картинку такого же класса
+    print("Getting the same class image from yadisk")
+    download(remote_last_same_img_path, local_last_same_img_path)
+    same_class_img = cv2.imread(local_last_same_img_path)
+
+    # читаем переданную в параметрах картинку
+    print("Reading input pic")
     contents = await image.read()
     nparr = np.fromstring(contents, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    input_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    #  обрабатываем
-    img = process(img)
+    # сохраняем на yadisk входную картинку
+    print("Saving input pic")
+    success = cv2.imwrite(local_img_path, input_img)
+    if success:
+        upload(local_img_path, remote_img_path)
+    else:
+        print(f"ERROR on imwrite {local_img_path}")
 
-    # отправляем в виде закодированной строки
-    _, encoded_img = cv2.imencode('.PNG', img)
+    # сохраняем на yadisk входные точки
+    print("Saving input json-points")
+    with open(local_json_path, "w+") as file:
+        file.write(input_points)
+    upload(local_json_path, remote_json_path)
+
+    # объединяем эти картинки нейронкой
+    print("processing pictures with nn")
+    result_img = await process(old_img=same_class_img, new_img=input_img)
+
+    # сохраняем на yadisk картинку-результат
+    print("saving result-pic on yadisk")
+    cv2.imwrite(local_last_same_img_path, result_img)
+    if success:
+        upload(local_last_same_img_path, remote_last_same_img_path)
+    else:
+        print(f"ERROR on imwrite {local_last_same_img_path}")
+
+    # отправляем картинку-результат в виде закодированной строки на фронт
+    print("sending result pic to front")
+    _, encoded_img = cv2.imencode('.PNG', result_img)
     encoded_img = base64.b64encode(encoded_img)
     return {"encoded_image": encoded_img}
+
+
+def yadisk_mkdir(path: str) -> None:
+    try:
+        my_disk.mkdir(path)
+    except ...:
+        pass
+
+
+def make_folders():
+    # локально
+    os.makedirs(SAVE_FOLDER + "top_left", exist_ok=True)
+    os.makedirs(SAVE_FOLDER + "top_right", exist_ok=True)
+    os.makedirs(SAVE_FOLDER + "bottom_left", exist_ok=True)
+    os.makedirs(SAVE_FOLDER + "bottom_left", exist_ok=True)
+
+    # на yadisk (более красивого способа не нашел)
+    yadisk_mkdir(YADISK_FOLDER + "top_left")
+    yadisk_mkdir(YADISK_FOLDER + "top_right")
+    yadisk_mkdir(YADISK_FOLDER + "bottom_left")
+    yadisk_mkdir(YADISK_FOLDER + "bottom_left")
 
 
 def parse_args():
